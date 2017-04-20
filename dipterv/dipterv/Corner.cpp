45fc5d75089b42ee4015ac621f53f8781b49f4c3
lala-PC\lala2 <tothlajosg@gmail.comm>
2017-04-20 18:06:30 +0200

    jo corner

--- dipterv/dipterv/Corner.cpp
+++ dipterv/dipterv/Corner.cpp
@@ -1,8 +1,77 @@
 #include "stdafx.h"
 #include "Corner.h"
 
-
-class Corner
+void Corner::printFingerPrint()
 {
 
-};
+}
+
+void Corner::run(std::vector<cv::Mat> keyFrames)
+{
+	for (int frame = 0; frame < keyFrames.size(); frame++)
+	{
+		cv::Mat SobelX;
+		cv::Mat SobelY;
+
+		cv::Sobel(keyFrames[frame], SobelX, CV_8UC1, 1, 0, 5);
+		cv::Sobel(keyFrames[frame], SobelY, CV_8UC1, 0, 1, 5);
+
+		for (int row = 5; row < keyFrames[frame].rows - 5; row ++)
+		{
+			for (int column = 5; column < keyFrames[frame].cols - 5; column++)
+			{
+				cv::Mat C(2, 2, CV_8UC1);
+				cv::Rect part;
+
+				part.x = column - 5;
+				part.width = 11;
+				part.y = row - 5;
+				part.height = 11;
+
+				cv::Mat dx = SobelX(part);
+				cv::Mat dy = SobelY(part);
+
+				//cv::waitKey(1);
+
+				float dxsum = cv::sum(dx)[0];
+				float dysum = cv::sum(dy)[0];
+
+				cv::Mat dxdy = dy.mul(dx);
+				float dxdysum = cv::sum(dxdy)[0];
+
+				C.at<char>(cv::Point(0, 0)) = dxsum;
+				C.at<char>(cv::Point(0, 1)) = dxdysum;
+				C.at<char>(cv::Point(1, 0)) = dxdysum;
+				C.at<char>(cv::Point(1, 1)) = dysum;
+
+				float trace = dxsum + dysum;
+				float det = dxsum * dysum - dxdysum * dxdysum;
+
+				float R = det - 0.06f * trace * trace;
+
+				if (R > 10000000)
+				{
+					cv::Point2i cornerCoord;
+					cornerCoord.x = column;
+					cornerCoord.y = row;
+					cornerPoints.push_back(cornerCoord);
+				}
+			}
+		}
+
+		for (auto c : cornerPoints)
+		{
+			cv::Point corner;
+			corner.x = c.x;
+			corner.y = c.y;
+			cv::circle(keyFrames[frame], corner, 5, cv::Scalar(0, 0, 255), 1);
+		}
+
+		cv::imshow("video", keyFrames[frame]);
+		
+		cv::waitKey(0);
+
+	}
+}
+
+
--- dipterv/dipterv/Corner.h
+++ dipterv/dipterv/Corner.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include <vector>
+#include <chrono>
+#include <iostream>
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+
+class Corner
+{
+public:
+	std::vector<cv::Point2i> cornerPoints;
+
+	void printFingerPrint();
+	void run(std::vector<cv::Mat> keyFrames);
+};
