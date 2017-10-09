e92071ced96e444a76ca7e6b2f73884b95c7ab6b
lala-PC\lala2 <tothlajosg@gmail.comm>
2017-10-09 14:41:42 +0200

    radial kész

--- dipterv/dipterv/RadialProjection.cpp
+++ dipterv/dipterv/RadialProjection.cpp
@@ -1,5 +1,18 @@
 #include "stdafx.h"
+#include <fstream>
 #include "RadialProjection.h"
+
+std::vector<std::string> split4(const std::string &text, char sep)
+{
+	std::vector<std::string> tokens;
+	std::size_t start = 0, end = 0;
+	while ((end = text.find(sep, start)) != std::string::npos) {
+		tokens.push_back(text.substr(start, end - start));
+		start = end + 1;
+	}
+	tokens.push_back(text.substr(start));
+	return tokens;
+}
 
 std::vector<cv::Point> RadialProjection::bresenham(cv::Point p0, cv::Point p1)
 {
@@ -148,126 +161,224 @@ void RadialProjection::printFingerPrint()
 {
 }
 
-void RadialProjection::run(std::vector<cv::Mat> keyFrames)
+std::string RadialProjection::checkMatchInDB(std::vector<std::vector<double>> fingerPrintToSearch)
 {
-	auto start = std::chrono::system_clock::now();
-	for (int keyFrame = 0; keyFrame < keyFrames.size(); keyFrame++)
-	{
-		float R[180];
-		float szoras[180];
-		float egyenesAtlag[180];
-		float szorasNegyzet[180];
-		
-		#pragma omp parallel for
-		for (int angle = 0; angle < 180; angle += 1)
-		{
-			float iranyVektorX = -1.0f;
-			float iranyVektorY = 0.0f;
+	loadDB();
 
-			rotateVector(&iranyVektorX, &iranyVektorY, (float)angle);
-			cv::Point2f intersectFloat;
-			cv::Point2f intersect2Float;
-			if (angle < 29.358f)
+	std::string bestVideo = "Nincs benne az adatbazisban";
+	float bestError = std::numeric_limits<double>::max();
+	for (auto video : dbFingerPrint)
+	{
+		std::string videoName = video.first;
+		float errorSum = 0.0f;
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 40; j++)
 			{
-				get_line_intersection(cv::Point2f(0.0f, 0.0f), cv::Point2f(0.0f, 719.0f), cv::Point2f(640.0f, 360.0f), cv::Point2f(640.0f + iranyVektorX * 10000.0f, 360.0f + iranyVektorY * 10000.0f), intersectFloat);
-				get_line_intersection(cv::Point2f(1279.0f, 0.0f), cv::Point2f(1279.0f, 719.0f), cv::Point2f(640.0f, 360.0f), cv::Point2f(640.0f + iranyVektorX * 10000.0f * -1.0f, 360.0f + iranyVektorY * 10000.0f * -1.0f), intersect2Float);
+				errorSum += fabs(fingerPrintToSearch[i][j] - video.second[i][j]);
 			}
-			else if (angle > 29.358f && angle < (180.0f - 29.358f))
+		}
+
+		if (errorSum < bestError && errorSum < 3) // 1 = threshold
+		{
+			bestError = errorSum;
+			bestVideo = videoName;
+		}
+	}
+
+	return bestVideo;
+}
+
+void RadialProjection::loadDB()
+{
+	std::vector<std::vector<double>> videoFingerPrints;
+	std::vector<double> fingerPrintNumbers;
+
+	std::string line;
+	std::ifstream myfile("Radial_database.txt");
+	int lineNum = 0;
+	std::string elozoVideoNev;
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			if (lineNum % 6 == 0)
 			{
-				get_line_intersection(cv::Point2f(0.0f, 0.0f), cv::Point2f(1279.0f, 0.0f), cv::Point2f(640.0f, 360.0f), cv::Point2f(640.0f + iranyVektorX * 10000.0f, 360.0f + iranyVektorY * 10000.0f), intersectFloat);
-				get_line_intersection(cv::Point2f(0.0f, 719.0f), cv::Point2f(1279.0f, 719.0f), cv::Point2f(640.0f, 360.0f), cv::Point2f(640.0f + iranyVektorX * 10000.0f * -1.0f, 360.0f + iranyVektorY * 10000.0f * -1.0f), intersect2Float);
+				if (videoFingerPrints.size() > 0)
+				{
+					dbFingerPrint[elozoVideoNev] = videoFingerPrints;
+					videoFingerPrints.clear();
+				}
+				elozoVideoNev = line;
 			}
 			else
 			{
-				get_line_intersection(cv::Point2f(1279.0f, 0.0f), cv::Point2f(1279.0f, 719.0f), cv::Point2f(640.0f, 360.0f), cv::Point2f(640.0f + iranyVektorX * 10000.0f, 360.0f + iranyVektorY * 10000.0f), intersectFloat);
-				get_line_intersection(cv::Point2f(0.0f, 0.0f), cv::Point2f(0.0f, 719.0f), cv::Point2f(640.0f, 360.0f), cv::Point2f(640.0f + iranyVektorX * 10000.0f * -1.0f, 360.0f + iranyVektorY * 10000.0f * -1.0f), intersect2Float);
+				std::vector<std::string> values = split4(line, ' ');
+				int frame = std::stoi(values[0]);
+				for (int i = 1; i < 40; i++)
+				{
+					fingerPrintNumbers.push_back(std::stod(values[i]));
+				}
+
+				videoFingerPrints.push_back(fingerPrintNumbers);
+				fingerPrintNumbers.clear();
 			}
 
-			cv::Point intersect = intersectFloat;
-			cv::Point intersect2 = intersect2Float;
-
-			std::vector<cv::Point> pixelsCoordinates = bresenham(intersect, intersect2);
-
-			float Mu = 0.0f;
-			float PLeftSide = 0.0f;
-			float PRightSide = 0.0f;
-			for (auto coordinate : pixelsCoordinates)
-			{
-				cv::Scalar pixelIntensity = keyFrames[keyFrame].at<uchar>(coordinate.y, coordinate.x);
-
-				PLeftSide += pixelIntensity[0] * pixelIntensity[0];
-				PRightSide += pixelIntensity[0];
-
-				Mu += pixelIntensity[0];
-			}
-
-			PLeftSide /= pixelsCoordinates.size();
-			PRightSide /= pixelsCoordinates.size();
-			Mu /= pixelsCoordinates.size();
-
-			PRightSide = PRightSide * PRightSide;
-
-			float P = PLeftSide - PRightSide;
-
-			szorasNegyzet[angle] = P;
-			egyenesAtlag[angle] = Mu;
-
-			float _szoras = 0.0f;
-
-			for (int i = 0; i < pixelsCoordinates.size(); i++)
-			{
-				cv::Scalar pixelIntensity = keyFrames[keyFrame].at<uchar>(pixelsCoordinates[i].y, pixelsCoordinates[i].x);
-
-				_szoras += (pixelIntensity[0] - Mu) * (pixelIntensity[0] - Mu);
-			}
-
-			_szoras /= pixelsCoordinates.size();
-			szoras[angle] = _szoras;
+			lineNum++;
 		}
 
-		float sumEgyenesAtlag = 0.0f;
-		for (auto o : egyenesAtlag)
+		myfile.close();
+	}
+
+	else std::cout << "Unable to open file";
+}
+
+std::vector<double> RadialProjection::run(cv::Mat keyFrame)
+{
+	auto start = std::chrono::system_clock::now();
+	float R[180];
+	float szoras[180];
+	float egyenesAtlag[180];
+	float szorasNegyzet[180];
+		
+	//#pragma omp parallel for
+	for (int angle = 0; angle < 180; angle += 1)
+	{
+		float iranyVektorX = -1.0f;
+		float iranyVektorY = 0.0f;
+
+		rotateVector(&iranyVektorX, &iranyVektorY, (float)angle);
+		cv::Point2f intersectFloat;
+		cv::Point2f intersect2Float;
+		if (angle < 29.358f)
 		{
-			sumEgyenesAtlag += o;
+			get_line_intersection(cv::Point2f(0.0f, 0.0f), cv::Point2f(0.0f, 719.0f), cv::Point2f(640.0f, 360.0f), cv::Point2f(640.0f + iranyVektorX * 10000.0f, 360.0f + iranyVektorY * 10000.0f), intersectFloat);
+			get_line_intersection(cv::Point2f(1279.0f, 0.0f), cv::Point2f(1279.0f, 719.0f), cv::Point2f(640.0f, 360.0f), cv::Point2f(640.0f + iranyVektorX * 10000.0f * -1.0f, 360.0f + iranyVektorY * 10000.0f * -1.0f), intersect2Float);
+		}
+		else if (angle > 29.358f && angle < (180.0f - 29.358f))
+		{
+			get_line_intersection(cv::Point2f(0.0f, 0.0f), cv::Point2f(1279.0f, 0.0f), cv::Point2f(640.0f, 360.0f), cv::Point2f(640.0f + iranyVektorX * 10000.0f, 360.0f + iranyVektorY * 10000.0f), intersectFloat);
+			get_line_intersection(cv::Point2f(0.0f, 719.0f), cv::Point2f(1279.0f, 719.0f), cv::Point2f(640.0f, 360.0f), cv::Point2f(640.0f + iranyVektorX * 10000.0f * -1.0f, 360.0f + iranyVektorY * 10000.0f * -1.0f), intersect2Float);
+		}
+		else
+		{
+			get_line_intersection(cv::Point2f(1279.0f, 0.0f), cv::Point2f(1279.0f, 719.0f), cv::Point2f(640.0f, 360.0f), cv::Point2f(640.0f + iranyVektorX * 10000.0f, 360.0f + iranyVektorY * 10000.0f), intersectFloat);
+			get_line_intersection(cv::Point2f(0.0f, 0.0f), cv::Point2f(0.0f, 719.0f), cv::Point2f(640.0f, 360.0f), cv::Point2f(640.0f + iranyVektorX * 10000.0f * -1.0f, 360.0f + iranyVektorY * 10000.0f * -1.0f), intersect2Float);
 		}
 
-		sumEgyenesAtlag /= 180.0f; //egyenesAtlag.size();
+		cv::Point intersect = intersectFloat;
+		cv::Point intersect2 = intersect2Float;
 
-		float sumSzorasAtlag = 0.0f;
-		for (auto o : szoras)
+		std::vector<cv::Point> pixelsCoordinates = bresenham(intersect, intersect2);
+
+		float Mu = 0.0f;
+		float PLeftSide = 0.0f;
+		float PRightSide = 0.0f;
+		for (auto coordinate : pixelsCoordinates)
 		{
-			sumSzorasAtlag += o;
+			cv::Scalar pixelIntensity = keyFrame.at<uchar>(coordinate.y, coordinate.x);
+
+			PLeftSide += pixelIntensity[0] * pixelIntensity[0];
+			PRightSide += pixelIntensity[0];
+
+			Mu += pixelIntensity[0];
 		}
 
-		sumSzorasAtlag /= 180.0f; //szoras.size();
+		PLeftSide /= pixelsCoordinates.size();
+		PRightSide /= pixelsCoordinates.size();
+		Mu /= pixelsCoordinates.size();
 
-		for (int i = 0; i < 180 /*egyenesAtlag.size()*/; i++)
+		PRightSide = PRightSide * PRightSide;
+
+		float P = PLeftSide - PRightSide;
+
+		szorasNegyzet[angle] = P;
+		egyenesAtlag[angle] = Mu;
+
+		float _szoras = 0.0f;
+
+		for (int i = 0; i < pixelsCoordinates.size(); i++)
 		{
-			float _R = (szorasNegyzet[i] - sumEgyenesAtlag) / sumSzorasAtlag;
+			cv::Scalar pixelIntensity = keyFrame.at<uchar>(pixelsCoordinates[i].y, pixelsCoordinates[i].x);
+
+			_szoras += (pixelIntensity[0] - Mu) * (pixelIntensity[0] - Mu);
+		}
+
+		_szoras /= pixelsCoordinates.size();
+		szoras[angle] = _szoras;
+	}
+
+	float sumEgyenesAtlag = 0.0f;
+	for (auto o : egyenesAtlag)
+	{
+		sumEgyenesAtlag += o;
+	}
+
+	sumEgyenesAtlag /= 180.0f; //egyenesAtlag.size();
+
+	float sumSzorasAtlag = 0.0f;
+	for (auto o : szoras)
+	{
+		sumSzorasAtlag += o;
+	}
+
+	sumSzorasAtlag /= 180.0f; //szoras.size();
+
+	for (int i = 0; i < 180 /*egyenesAtlag.size()*/; i++)
+	{
+		float _R = (szorasNegyzet[i] - sumEgyenesAtlag) / sumSzorasAtlag;
+		if (fabs(sumSzorasAtlag) < 1e-8)
+		{
+			R[i] = 0.0f;
+		}
+		else
+		{
 			R[i] = _R;
 		}
+	}
 
-		// DCT
-		float sum = 0.0f;
-		float N = 180;
-		std::vector<float> coeffs;
-		for (int n = 0; n < 40; n++)
+	// DCT
+	float sum = 0.0f;
+	float N = 180;
+	std::vector<double> coeffs;
+	for (int n = 0; n < 40; n++)
+	{
+		sum = 0.0f;
+		for (int phi = 0; phi < 180 /*R.size()*/ - 1; phi++)
 		{
-			sum = 0.0f;
-			for (int phi = 0; phi < 180 /*R.size()*/ - 1; phi++)
-			{
-				sum += R[phi] * cos((M_PI * ((2 * phi) + 1) * n) / (2 * N));
-			}
-
-			float D = sqrt(2 / N) * sum;
-			coeffs.push_back(D);
+			sum += R[phi] * cos((M_PI * ((2 * phi) + 1) * n) / (2 * N));
 		}
 
-		fingerPrint.push_back(coeffs);
+		float D = sqrt(2 / N) * sum;
+		coeffs.push_back(D);
 	}
+
+	fingerPrint.push_back(coeffs);
+
+	return coeffs;
 
 	auto end = std::chrono::system_clock::now();
 	auto elapsed = std::chrono::duration_cast<std::chrono::seconds>(end - start);
 	double elapsed_seconds = std::chrono::duration_cast<std::chrono::duration<double>>(end - start).count();
 	std::cout << " Radial runtime: " << elapsed_seconds << " sec" << std::endl;
+}
+
+void RadialProjection::appendToDatabase(int n, std::vector<int> keyFrameNumbers)
+{
+	std::ofstream dbFile;
+	dbFile.open("Radial_database.txt", std::ios::out | std::ios::app);
+	dbFile << std::to_string(n) + ".mp4" << "\n";
+
+	for (int i = 0; i < 5; i++)
+	{
+		dbFile << keyFrameNumbers[i] << " ";
+		for (auto radial : fingerPrint[i])
+		{
+			dbFile << radial << " ";
+		}
+
+		dbFile << "\n";
+	}
+
+	dbFile.close();
 }
--- dipterv/dipterv/RadialProjection.h
+++ dipterv/dipterv/RadialProjection.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <chrono>
 #include <iostream>
+#include <map>
 #include "opencv2/objdetect/objdetect.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
@@ -10,14 +11,18 @@
 
 class RadialProjection
 {
-private:
-	std::vector<std::vector<float>> fingerPrint;
 public:
+	std::vector<std::vector<double>> fingerPrint;
+	std::map<std::string, std::vector<std::vector<double>>> dbFingerPrint;
+
 	std::vector<cv::Point> bresenham(cv::Point p0, cv::Point p1);
 	int get_line_intersection(float p0_x, float p0_y, float p1_x, float p1_y, float p2_x, float p2_y, float p3_x, float p3_y, float *i_x, float *i_y);
 	int get_line_intersection(cv::Point2f p0, cv::Point2f p1, cv::Point2f p2, cv::Point2f p3, cv::Point2f& interSection);
 	void rotateVector(float *x, float *y, float angle);
 
 	void printFingerPrint();
-	void run(std::vector<cv::Mat> keyFrames);
+	std::string checkMatchInDB(std::vector<std::vector<double>> fingerPrintToSearch);
+	void loadDB();
+	std::vector<double> run(cv::Mat keyFrames);
+	void appendToDatabase(int n, std::vector<int> keyFrameNumbers);
 };
